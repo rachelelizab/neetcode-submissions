d732143beab2edb3b2828c282027f97a1f7f20e9
RACHEL ELIZABETH JOY <rachelelizabethjoy@gmail.com>
2026-08-30 14:29:39 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self,nums:list[int])->bool:
+        hashsets=set()
+        for n in nums:
+            if n in hashsets:
+                return True
+            hashsets.add(n)
+        return False  
+
+
